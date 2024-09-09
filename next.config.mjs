f622fce3896f60d2d0925c2010d6761f50a3f158
joser27 <joser27@uw.edu>
2024-09-09 12:12:31 -0700

    Update Next.js config for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/next-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/next-portfolio/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
