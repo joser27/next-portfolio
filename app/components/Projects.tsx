df2089b8d93a191118da90b36f1af1fdf8202520
joser27 <joser27@uw.edu>
2025-03-20 12:34:39 -0700

    correct command has been deprecated.

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function Projects() {
   const projects = [
     {
@@ -112,10 +114,11 @@ export default function Projects() {
             className="bg-[rgb(41,41,41)] rounded-xl shadow-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:shadow-2xl"
           >
             <div className="relative h-56 bg-gray-700">
-              <img
+              <Image
                 src={project.gifUrl}
                 alt={project.title}
-                className="absolute w-full h-full object-cover object-center"
+                fill
+                className="object-cover object-center"
               />
             </div>
             <div className="p-6">
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,28 @@ export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen bg-[rgb(31,31,31)]">
       <NavBar />
-      <main className="flex-grow">
-        <Home />
-        <Projects />
-        <Contact />
-      </main>
+      <div className="flex flex-row w-full">
+        {/* Left Sidebar */}
+        <div className="w-2/6 min-h-screen bg-blue-500/20 p-4">
+          <div className="fixed">
+            Left Section
+          </div>
+        </div>
+
+        {/* Main Content (Middle) */}
+        <main className="w-2/6 min-h-screen bg-green-500/20 p-4">
+          <Home />
+          <Projects />
+          <Contact />
+        </main>
+
+        {/* Right Sidebar */}
+        <div className="w-2/6 min-h-screen bg-red-500/20 p-4">
+          <div className="fixed">
+            Right Section
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
